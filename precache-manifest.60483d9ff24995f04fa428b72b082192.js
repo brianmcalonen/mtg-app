9a773686aaa5c9665d5c8d28c5eb50e71d9f4f1e
brianmcalonen <brianm0623@gmail.com>
2019-09-05 08:37:06 -0700

    Updates

---
+++ precache-manifest.60483d9ff24995f04fa428b72b082192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d661eb2056796d6e582",
+    "url": "/mtg-app/static/js/runtime~main.7d661eb2.js"
+  },
+  {
+    "revision": "d43a9b6d8b4c55bdec16",
+    "url": "/mtg-app/static/js/main.d43a9b6d.chunk.js"
+  },
+  {
+    "revision": "1451762bfb2ec2d9f8c5",
+    "url": "/mtg-app/static/js/2.1451762b.chunk.js"
+  },
+  {
+    "revision": "d43a9b6d8b4c55bdec16",
+    "url": "/mtg-app/static/css/main.1ec59914.chunk.css"
+  },
+  {
+    "revision": "820cb235ff7477e3625d778d5bc29775",
+    "url": "/mtg-app/index.html"
+  }
+];
